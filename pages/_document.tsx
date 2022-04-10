1769e8ba0487eb83ae2f57a32ef5fa6c22e9bccd
ramanavami <ramanavami32@gmail.com>
2022-04-10 14:09:31 +0930

    finished design 2 sections

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,17 +7,10 @@ import Document, {
 } from "next/document";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx);
-
-    return initialProps;
-  }
-
   render() {
     return (
       <Html>
         <Head>
-        <title>Morii</title>
         <link rel="preconnect" href="https://fonts.googleapis.com"></link>
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""></link>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,115 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
+import type { NextPage } from "next";
+import Head from "next/head";
+import styled from "styled-components";
+
+const WelcomeBanner = styled.div`
+  height: 100%;
+`;
+
+const TitleBanner = styled.h1`
+  font-size: 220px;
+  margin: 0;
+`;
+
+const DescBanner = styled.h3`
+  font-size: 73px;
+  margin: 0;
+  font-weight: 500;
+`;
+
+const MoriiHello = styled(TitleBanner)`
+  font-size: 98px;
+`;
+const MoriiCreator = styled(DescBanner)`
+  font-size: 45px;
+`;
+
+const MoriiLetter = styled.p`
+  font-size: 30px;
+  letter-spacing: 3.5px;
+`;
+
+const MoriiIntroduction = styled(WelcomeBanner)`
+  padding: 10%;
+  position: relative;
+`;
+
+const PageIndicator = styled.div`
+  position: absolute;
+  text-align: right;
+  font-weight: 500;
+  font-size: 20px;
+  bottom: 20px;
+  right: 30px;
+  padding-bottom : 5px;
+
+  &:after {
+    content : '';
+    border-bottom: 2px solid black;
+    position: absolute;
+    width: 25%;
+    height : 2px;
+    bottom: 0px;
+    right: 0px;
+    transition : width 0.6s;
+  }
+
+  &:hover:after{
+    width : 100%;
+    transition: width 0.6s;
+  }
+
+`;
+
+// const PageTitle = styled.p`
+//   bottom: 20px;
+//   right: 30px;
+//   text-align: right;
+//   font-weight: 500;
+//   font-size: 20px;
+//   position: absolute;
+//   margin-bottom: 9px;
+// `;
 
 const Home: NextPage = () => {
   return (
-    <div>
-    </div>
-  )
-}
+    <>
+      <Head>
+        <title>Morii</title>
+      </Head>
+      <WelcomeBanner className="flex flex-column x-center y-center">
+        <TitleBanner>Morii.</TitleBanner>
+        <DescBanner>A place for you to lean on tired day</DescBanner>
+      </WelcomeBanner>
+      <MoriiIntroduction className="flex y-center">
+        <div>
+          <MoriiHello>Hello</MoriiHello>
+          <MoriiCreator>
+            My name is <b>Morii Shikaa</b>
+          </MoriiCreator>
+          <MoriiLetter>
+            The Creator of the forest full of pages <br />
+            filled with the outpouring of hearts and thoughts. <br />
+            <br />
+            The pages that have been provided in this forest are <br />
+            intended for everyone who just wants to visit, relax and <br />
+            just want to know what&apos;s on the mind of a loner at night.
+            <br />
+            <br />
+            I hope you like this forest and visit this place anew in whatever
+            <br />
+            condition your heart is in. Nice to know you, hope you like <br />
+            this forest and thank you for visiting or even intend <br />
+            to come back.
+          </MoriiLetter>
+        </div>
+        <PageIndicator>
+          warm welcome from <br />
+          the forest
+        </PageIndicator>
+      </MoriiIntroduction>
+    </>
+  );
+};
 
-export default Home
+export default Home;
